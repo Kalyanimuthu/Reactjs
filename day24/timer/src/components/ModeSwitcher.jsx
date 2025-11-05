9961b1814b5dac3a00c49799968b2c064b3a9af9
Kalyanimuthu <kalyaniram93@gmail.com>
2025-11-05 16:49:36 +0530

    Delete day24 directory

--- day24/timer/src/components/ModeSwitcher.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ModeSwitcher({ mode, onSwitch }) {
-  return (
-    <button onClick={onSwitch} className="mt-6 text-sm text-blue-600 underline">
-      Switch to {mode === "work" ? "Break" : "Work"} Mode
-    </button>
-  );
-}
